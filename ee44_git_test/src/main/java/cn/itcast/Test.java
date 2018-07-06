20ec0770b7d774824ac4d698899b39ec36b60d3f
sublun <sublun@126.com>
2018-07-06 19:49:18 +0800

    clone提交

--- ee44_git_test/src/main/java/cn/itcast/Test.java
+++ ee44_git_test/src/main/java/cn/itcast/Test.java
@@ -4,5 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
